c524faf4adc5f0ed93b39713990f3d0014158021
joemurilloa <joemurillo95@gmail.com>
2025-03-17 11:11:19 -0600

    netlify para que funcione intento3

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: []
-    },
-    commonjsOptions: {
-      include: [],
-    }
-  },
-  optimizeDeps: {
-    exclude: ['@rollup/rollup-linux-x64-gnu']
+    minify: 'terser'
   }
 });
